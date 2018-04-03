3104af99189e0dda13d583256bf65b89b68c0df5
encry1024 <register.chihiro@gmail.com>
2018-04-03 21:21:32 +0900

    add domain report

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -2,3 +2,7 @@ pub mod url {
     pub static scan: &str = "https://www.virustotal.com/vtapi/v2/url/scan";
     pub static report: &str = "https://www.virustotal.com/vtapi/v2/url/report";
 }
+
+pub mod domain {
+    pub static report: &str = "https://www.virustotal.com/vtapi/v2/domain/report";
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use std::collections::HashMap;
     
 pub mod url;
 pub mod api;
+pub mod domain;
 
 #[derive(Debug,Deserialize)]
 pub struct ScanResponse {
@@ -40,4 +41,26 @@ pub struct ReportResponse {
     pub positives: Option<u32>,
     pub total: Option<u32>,
     pub scans: Option<HashMap<String, FileScan>>
+}
+
+#[derive(Debug,Deserialize)]
+pub struct Resolutions {
+    pub last_resolved: String,
+    pub ip_address: String
+}
+
+#[derive(Debug,Deserialize)]
+pub struct DetectedUrls {
+    pub url: String,
+    pub positives: u32,
+    pub total: u32,
+    pub scan_date: String,
+}
+
+#[derive(Debug,Deserialize)]
+pub struct DomainResponse {
+    pub response_code: u32,
+    pub verbose_msg: String,
+    pub resolutions: Vec<Resolutions>,
+    pub detected_urls: Vec<DetectedUrls>
 }
