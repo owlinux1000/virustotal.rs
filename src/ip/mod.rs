04b79c8789b427f17a6427c2294825087c389d8a
owlinux1000 <encry1024@gmail.com>
2019-11-03 13:27:33 +0900

    Update the function using /ip-address/repot

--- src/ip/mod.rs
+++ src/ip/mod.rs
@@ -1,27 +1,27 @@
 use reqwest::Client;
 use serde_json::from_str;
-use super::*;
-
-/// Retrieve an IP address report
-///
-/// # Example
-/// 
-/// ```
-/// use virustotal::*;
-/// 
-/// let api_key = "Your API key";
-/// let ip_address = "the IP address you want to check";
-/// ip::report(api_key, domain);
-/// ```
-pub fn report(api_key: &str, ip_address: &str) -> IpAddressReportResponse {
-
-    let params: &str = &format!("?apikey={}&ip={}", &api_key, &ip_address);
-    let url = ["https://www.virustotal.com/vtapi/v2/ip-address/report", params].join("");
-    let mut resp = Client::new()
-        .get(&url)
-        .send()
-        .unwrap();
-
-    let text: &str = &resp.text().unwrap();
-    from_str(&text).unwrap()
+use super::{VtClient, IpAddressReportResponse};
+impl <'a>VtClient<'a> {
+    /// Retrieve an IP address report
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use virustotal::*;
+    /// 
+    /// let vt = VtClient::new("Your API Key");
+    /// vt.report_ip_address("192.168.2.1");
+    /// ```
+    pub fn report_ip_address(self, ip_address: &str) -> IpAddressReportResponse {
+        let params: &str = &format!(
+            "?apikey={}&ip={}", self.api_key, &ip_address
+        );
+        let url = [self.endpoint, "/ip-address/report", params].join("");
+        let mut resp = Client::new()
+            .get(&url)
+            .send()
+            .unwrap();
+        let text: &str = &resp.text().unwrap();
+        from_str(&text).unwrap()
+    }            
 }
