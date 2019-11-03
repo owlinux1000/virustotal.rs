f6ce822ff82ae4ab9e6e0d40afd0bd8c5e25bfff
owlinux1000 <encry1024@gmail.com>
2019-11-03 14:46:18 +0900

    Update the functoin using /url/scan, report

--- src/url/mod.rs
+++ src/url/mod.rs
@@ -1,51 +1,52 @@
 use reqwest::Client;
 use serde_json::from_str;
-use super::*;
+use super::{VtClient, UrlScanResponse, UrlReportResponse};
 
-/// Scan an URL
-///
-/// # Example
-/// 
-/// ```
-/// use virustotal::*;
-/// 
-/// let api_key = "Your API key";
-/// let url = "the URL you want to scan";
-/// url::scan(api_key, url);
-/// ```
-pub fn scan(api_key: &str, url: &str) -> UrlScanResponse {
-    
-    let mut resp = Client::new()
-        .post("https://www.virustotal.com/vtapi/v2/url/scan")
-        .form(&[("apikey", &api_key), ("url", &url)])
-        .send()
-        .unwrap();
-    
-    let text: &str = &resp.text().unwrap();
-    from_str(&text).unwrap()
-        
-}
+impl <'a>VtClient<'a> {
+    /// Scan an URL
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use virustotal::*;
+    /// 
+    /// let vt = VtClient::new("Your API Key");
+    /// let url = "https://example.com";
+    /// vt.scan_url(url);
+    /// ```
+    pub fn scan_url(self, url: &str) -> UrlScanResponse {
+        let url = &[self.endpoint, "/url/scan"].join("");
+        let mut resp = Client::new()
+            .post(url)
+            .form(&[("apikey", self.api_key), ("url", &url)])
+            .send()
+            .unwrap();
+        let text: &str = &resp.text().unwrap();
+        from_str(&text).unwrap()
+    }
 
-/// Retrieve URL scan reports
-///
-/// # Example
-/// 
-/// ```
-/// use virustotal::*;
-/// 
-/// let api_key = "Your API key";
-/// let resource = "the resource you want to check";
-/// url::report(api_key, resource);
-/// ```
-pub fn report(api_key: &str, resource: &str) -> UrlReportResponse {
+    /// Retrieve URL scan reports
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use virustotal::*;
+    ///
+    /// let vt = VtClient::new("Your API Key");
+    /// vt.report_url(resource);
+    /// ```
+    pub fn report_url(self, resource: &str) -> UrlReportResponse {
 
-    let params: &str = &format!("?apikey={}&resource={}", &api_key, &resource);
-    let url = ["https://www.virustotal.com/vtapi/v2/url/report", params].join("");
-    let mut resp = Client::new()
-        .get(&url)
-        .send()
-        .unwrap();
+        let params: &str = &format!(
+            "?apikey={}&resource={}", self.api_key, &resource
+        );
+        let url = &[self.endpoint, "/url/report", params].join("");
+        let mut resp = Client::new()
+            .get(url)
+            .send()
+            .unwrap();
     
-    let text: &str = &resp.text().unwrap();
-    from_str(&text).unwrap()
+        let text: &str = &resp.text().unwrap();
+        from_str(&text).unwrap()
+    }
 }
