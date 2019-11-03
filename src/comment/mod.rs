88bf6e85ff064195deee3493a158e70b6026db69
owlinux1000 <encry1024@gmail.com>
2019-11-03 14:23:04 +0900

    Update the function using /comment/get,put

--- src/comment/mod.rs
+++ src/comment/mod.rs
@@ -1,43 +1,54 @@
+//use std::collections::HashMap;
 use reqwest::Client;
 use serde_json::from_str;
-use super::*;
+use super::{VtClient, CommentGetResponse, CommentPutResponse};
 
-/// Make comments on file and URLs
-///
-/// # Example
-/// 
-/// ```
-/// use virustotal::*;
-/// 
-/// let api_key = "Your API key";
-/// let resource = "the resource you want to put comments";
-/// let comment = "This is a test";
-/// comment::put(api_key, resource, comment);
-/// ```
-pub fn put(api_key: &str, resource: &str, comment: &str) -> CommentPutResponse {
+impl <'a>VtClient<'a> {
+    /// Make comments on file and URLs
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use virustotal::*;
+    /// 
+    /// let vt = VtClient::new("Your API Key");
+    /// let resource = "the resource you want to put comments";
+    /// let comment = "This is a test";
+    /// vt.put(resource, comment);
+    /// ```
+    pub fn put(self, resource: &str, comment: &str) -> CommentPutResponse {
+        let url = &[self.endpoint, "/comments/put"].join("");
+        let mut resp = Client::new()
+            .post(url)
+            .form(&[("apikey", self.api_key), ("comment", &comment), ("resource", &resource)])
+            .send()
+            .unwrap();
     
-    let mut resp = Client::new()
-        .post("https://www.virustotal.com/vtapi/v2/comments/put")
-        .form(&[("apikey", &api_key), ("comment", &comment), ("resource", &resource)])
-        .send()
-        .unwrap();
+        let text: &str = &resp.text().unwrap();
+        from_str(&text).unwrap()
+    }
     
-    let text: &str = &resp.text().unwrap();
-    from_str(&text).unwrap()
-        
+    /// Get comments for a file or URL
+    ///
+    /// # Example
+    /// 
+    /// ```
+    /// use virustotal::*;
+    /// 
+    /// let vt = VtClient::new("Your API Key");
+    /// let resource = "the resource you want to get comments";
+    /// vt.get(comment);
+    /// ```
+    pub fn get(self, resource: &str) -> CommentGetResponse {
+        let params: &str = &format!(
+            "?apikey={}&resource={}", self.api_key, &resource
+        );
+        let url = &[self.endpoint, "/comments/get", params].join("");
+        let mut resp = Client::new()
+            .get(url)
+            .send()
+            .unwrap();
+        let text: &str = &resp.text().unwrap();
+        from_str(&text).unwrap()
+    }
 }
-
-pub fn get(api_key: &str, resource: &str) -> CommentGetResponse {
-
-    let params: &str = &format!("?apikey={}&resource={}", &api_key, &resource);
-    let url = ["https://www.virustotal.com/vtapi/v2/comments/get", params].join("");
-    let mut resp = Client::new()
-        .get(&url)
-        .send()
-        .unwrap();
-    
-    let text: &str = &resp.text().unwrap();
-    from_str(&text).unwrap()
-}
-
-
